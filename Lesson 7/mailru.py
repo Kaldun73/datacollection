4fc0f216df1360bea3d501b5cb78b9e8ca1d4b91
Kaldun73 <vladimirkaldin@gmail.com>
2020-05-07 15:19:55 +0300

    7th lesson update

--- Lesson 7/mailru.py
+++ Lesson 7/mailru.py
@@ -3,45 +3,67 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from pymongo import MongoClient
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import time
+
+def letter_to_db(letter: {}):
+    client = MongoClient('localhost', 27017)
+    db = client['mailru']
+    collection = db.mailru
+    collection.update_one({'date': letter['date']}, {'$set': letter}, upsert=True)
 
 
-client = MongoClient('localhost', 27017)
-db = client['mailru']
-collection = db.mailru
+def scroll():
+    time.sleep(2)
+    emails = WebDriverWait(driver, 10).until(
+        EC.presence_of_all_elements_located((By.CLASS_NAME, 'llc__container'))
+    )
+    action = ActionChains(driver)
+    action.move_to_element(emails[-1])
+    action.perform()
 
-options = Options()
-options.add_argument('start-maximized')
-driver = webdriver.Chrome(options=options)
-driver.get('https://mail.ru/')
-assert "Mail.ru" in driver.title
+def letter_collector(driver):
+    letter = {}
+    letter['sender'] = driver.find_element_by_class_name('ll-crpt').get_attribute('title')
+    letter['date'] = driver.find_element_by_css_selector('div.llc__item_date').get_attribute('title')
+    letter['topic'] = driver.find_element_by_css_selector('span.llc__subject').text
+    letter['desc'] = driver.find_element_by_css_selector('span.ll-sp__normal').text
+    return letter
 
-elem = driver.find_element_by_id('mailbox:login')
-elem.send_keys('study.ai_172@mail.ru')
-elem.send_keys(Keys.ENTER)
+if __name__ == '__main__':
+    options = Options()
+    options.add_argument('start-maximized')
+    driver = webdriver.Chrome(options=options)
+    driver.get('https://mail.ru/')
 
-elem = driver.find_element_by_id('mailbox:password')
-elem.send_keys('NewPassword172')
-elem.send_keys(Keys.ENTER)
+    elem = driver.find_element_by_id('mailbox:login')
+    elem.send_keys('study.ai_172@mail.ru')
+    elem.send_keys(Keys.ENTER)
 
-# for i in range(15):
-#     emails = driver.find_elements_by_css_selector('div.llc__content llc__content_pony-mode')
-#     action = ActionChains(driver)
-#     action.move_to_element(emails[-1])
-#     action.perform()
+    elem = driver.find_element_by_id('mailbox:password')
+    elem.send_keys('NewPassword172')
+    elem.send_keys(Keys.ENTER)
 
-driver.find_element_by_class_name('llc__content llc__content_pony-mode').send_keys(Keys.PAGE_DOWN)
+    emails = WebDriverWait(driver, 10).until(
+        EC.presence_of_all_elements_located((By.CLASS_NAME, 'llc__content'))
+    )
+    while True:
+        emails = driver.find_elements_by_class_name('llc__container')
+        last_email = emails[-1]
+        for email in emails:
+            letter = letter_collector(email)
+            letter_to_db(letter)
+        scroll()
+        scrolled_emails = driver.find_elements_by_class_name('llc__container')
+        last_scrolled_email = scrolled_emails[-1]
+        if last_email == last_scrolled_email:
+            break
+
+    driver.quit()
+    print('Письма собраны!')
 
 
-emails = driver.find_elements_by_class_name('llc__content llc__content_pony-mode')
 
-letter = {}
-for email in emails:
-    dict['sender'] = email.find_element_by_css_selector('span.ll-crpt').text
-    dict['date'] = email.find_element_by_css_selector('div.llc__item llc__item_date').text
-    dict['topic'] = email.find_element_by_css_selector('span.llc__subject').text
-    dict['desc'] = email.find_element_by_css_selector('span.llc__snippet').text
-
-    collection.update_one({'$set': letter}, upsert=True)
-
-driver.quit()
 
